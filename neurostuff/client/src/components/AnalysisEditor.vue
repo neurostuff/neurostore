06513a1d7045d1e6084f453bf7440a493d3daf1a
Tal Yarkoni <tyarkoni@gmail.com>
2019-05-05 19:39:53 -0500

    rename model components to editor

---
+++ neurostuff/client/src/components/AnalysisEditor.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container" id="form">
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <vue-form-generator :schema="schema" :model="model" :options="formOptions"></vue-form-generator>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ajaxHandler from './mixins/ajaxHandler';
+
+export default {
+  mixins: [ajaxHandler],
+  data() {
+    return {
+      resource: 'analyses',
+      model: {},
+      schema: {
+        fields: [{
+          type: "input",
+          inputType: "text",
+          label: "Name",
+          model: "name",
+          // readonly: true,
+          featured: true,
+          styleClasses: "title",
+        }, {
+          type: "input",
+          inputType: "text",
+          label: "Description",
+          model: "description",
+          // readonly: true,
+        }, {
+          type: "input",
+          inputType: "text",
+          label: "Publication",
+          model: "publication",
+          // readonly: true,
+        }, {
+          type: "input",
+          inputType: "text",
+          label: "DOI",
+          model: "doi",
+          // readonly: true,
+        }, {
+          type: "textArea",
+          label: "Metadata",
+          model: "metadata",
+          rows: 10
+        }, {
+          type: "submit",
+          buttonText: "Save",
+          onSubmit: this.saveModel,
+        }
+        ],
+      },
+    };
+  },
+  methods: {
+  },
+};
+</script>
---
+++ neurostuff/client/src/components/ImageEditor.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container" id="form">
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <vue-form-generator :schema="schema" :model="model"
+                            :options="formOptions"></vue-form-generator>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ajaxHandler from './mixins/ajaxHandler';
+
+export default {
+  mixins: [ajaxHandler],
+  data() {
+    return {
+      resource: 'images',
+      schema: {
+        fields: [{
+          type: "input",
+          inputType: "text",
+          label: "URL",
+          model: "path",
+          // readonly: true,
+          featured: true,
+          styleClasses: "title",
+        }, {
+          type: "input",
+          inputType: "text",
+          label: "Space",
+          model: "space",
+          // readonly: true,
+        }, {
+          type: "input",
+          inputType: "text",
+          label: "Value type",
+          model: "value_type",
+          // readonly: true,
+        }, {
+          type: "input",
+          inputType: "text",
+          label: "Analysis IRI",
+          model: "analysis",
+          // readonly: true,
+        }, {
+          type: "submit",
+          buttonText: "Save",
+          onSubmit: this.saveModel,
+        }
+        ],
+      },
+    };
+  },
+  methods: {
+  },
+};
+</script>
