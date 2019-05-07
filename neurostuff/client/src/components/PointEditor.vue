57cf818f28c0ca1dc851514cdce07bef30f75eb5
Tal Yarkoni <tyarkoni@gmail.com>
2019-05-07 18:28:16 -0500

    add delete row functionality

--- neurostuff/client/src/components/PointEditor.vue
+++ neurostuff/client/src/components/PointEditor.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <h3>Point Editor</h3>
+
     <div id="add-col">
+      <b-button size="sm" v-on:click="addRow">Add row</b-button>
       <b-button size="sm" v-b-modal.add-col-modal>Add column</b-button>
     </div>
+
     <b-modal id="add-col-modal" title="Add new column" @ok="addColumn">
       <span>New column name:</span>
       <input type="text" v-model="newColName" />
@@ -11,8 +14,9 @@
         {{newColModalWarning}}
       </b-alert>
     </b-modal>
-    <b-table striped small responsive :items="model"
-             :fields="allFields">
+
+    <b-table striped small responsive :items="model" :fields="allFields"
+             primary-key="@id">
       <template v-for="(pf, index) in permaFields" :slot="pf" slot-scope="data">
         <input type="text" v-model="data.item.coordinates[index]"
          style="max-width: 60px;" />
@@ -20,6 +24,9 @@
       <template v-for="ef in extraFields" :slot="ef" slot-scope="data">
         <input type="text" v-model="data.item.values[ef]"
         style="width;" />
+      </template>
+      <template slot="delete" slot-scope="data">
+         <unicon name="trash-alt" fill="royalblue" v-on:click="deleteRow(data.item['@id'])" />
       </template>
     </b-table>
   </div>
@@ -33,9 +40,9 @@ export default {
       permaFields: ['X', 'Y', 'Z'],
       extraFields: [],
       model: [
-        {coordinates: [-12, 24, 8], values: { p: 0.002, t: 2.85}},
-        {coordinates: [32, 17, 41], values: { p: 0.08, t: 1.8}},
-        {coordinates: [8, -3, 0], values: { p: 0.8, t: -0.7}},
+        {'@id': 4, coordinates: [-12, 24, 8], values: { p: 0.002, t: 2.85}},
+        {'@id': 'u', coordinates: [32, 17, 41], values: { p: 0.08, t: 1.8}},
+        {'@id': 'a', coordinates: [8, -3, 0], values: { p: 0.8, t: -0.7}},
       ],
       newColName: '',
       newColModalWarning: false
@@ -72,8 +79,12 @@ export default {
       this.updateFields();
     },
     addRow() {
-
+      this.model.push({coordinates: [0, 0, 0], values: {}})
     },
+    deleteRow(id) {
+      const index = this.model.findIndex(item => item['@id'] === id);
+      this.model.splice(index, 1);
+    }
   },
   mounted() {
     // call this explicitly when we need to, because we're abusing v-model by
@@ -82,7 +93,7 @@ export default {
     this.updateFields();
   },
   computed: {
-    allFields() { return this.permaFields.concat(this.extraFields) }
+    allFields() { return this.permaFields.concat(this.extraFields, ['delete']) }
   }
 };
 </script>
--- neurostuff/client/src/main.js
+++ neurostuff/client/src/main.js
@@ -2,10 +2,16 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
+import Unicon from 'vue-unicons';
+import { uniTrashAlt } from 'vue-unicons/src/icons';
+
 import App from './App';
 import router from './router';
 
 Vue.use(BootstrapVue);
+
+Unicon.add([uniTrashAlt]);
+Vue.use(Unicon);
 
 Vue.config.productionTip = false;
 
