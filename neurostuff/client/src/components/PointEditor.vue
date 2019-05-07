6e5fedb66a7f99cfc4137466864e2361fe664b12
Tal Yarkoni <tyarkoni@gmail.com>
2019-05-07 17:15:24 -0500

    add 'new column' button and modal

--- neurostuff/client/src/components/PointEditor.vue
+++ neurostuff/client/src/components/PointEditor.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h3>Point Editor</h3>
+    <div id="add-col">
+      <b-button size="sm" v-b-modal.add-col-modal>Add column</b-button>
+      <b-modal id="add-col-modal" title="Add new column" @ok="addColumn">
+        <p>New column name:</p>
+        <input type="text" v-model="newColName" />
+      </b-modal>
+    </div>
     <b-table striped small responsive :items="model"
              :fields="permafields.concat(extrafields)">
       <template v-for="(pf, index) in permafields" :slot="pf" slot-scope="data">
@@ -26,7 +33,8 @@ export default {
         {coordinates: [-12, 24, 8], values: { p: 0.002, t: 2.85}},
         {coordinates: [32, 17, 41], values: { p: 0.08, t: 1.8}},
         {coordinates: [8, -3, 0], values: { p: 0.8, t: -0.7}},
-      ]
+      ],
+      newColName: ''
     };
   },
   methods: {
@@ -34,13 +42,30 @@ export default {
       const reducer = (acc, c) => acc.concat(Object.keys(c.values))
       let fields = this.model.reduce(reducer, []);
       this.extrafields = [...new Set(fields)];
-    }
+    },
+    addColumn(evt) {
+      this.model.forEach((point) => {
+        point.values[this.newColName] = null;
+      });
+      this.newColName = '';
+      this.updateFields();
+    },
+    addRow() {
+
+    },
   },
   mounted() {
+    // call this explicitly when we need to, because we're abusing v-model by
+    // binding input fields. if we made this a computed prop, it would trigger
+    // on every keystroke edit in the table.
     this.updateFields();
-    console.log("YO!");
-    console.log(this.permafields);
-    console.log(this.extrafields);
   }
 };
 </script>
+
+<style>
+#add-col {
+  text-align: right;
+  margin-bottom: 5px;
+}
+</style>
