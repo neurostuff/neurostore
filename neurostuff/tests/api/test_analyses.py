290cda429219cf18032b811fbb2b0bd2399bd675
delavega4 <aleph4@gmail.com>
2020-09-01 13:45:07 -0500

    Add tests for analyses

--- neurostuff/tests/api/test_analyses.py
+++ neurostuff/tests/api/test_analyses.py
@@ -5,5 +5,23 @@ def test_get_analyses(auth_client, ingest_neurosynth):
     # List of datasets
     resp = auth_client.get('/api/analyses/')
     assert resp.status_code == 200
-    dataset_list = decode_json(resp)
-    assert type(dataset_list) == list
+    analysis_list = decode_json(resp)
+    assert type(analysis_list) == list
+
+    assert len(analysis_list) == 6
+
+    # Check dataset keys
+    analysis = analysis_list[0]
+    keys = ['@context', '@id', '@type', 'condition', 'created_at', 'image',
+            'name', 'point', 'study', 'weight']
+    for k in keys:
+        assert k in analysis
+
+    assert analysis['@context'] == {'@vocab': 'http://neurostuff.org/nimads/'}
+    assert analysis['@type'] == 'Analysis'
+    a_id = analysis['@id'].split('/')[-1]
+
+    # Query specify analysis ID
+    resp = auth_client.get(f"/api/analyses/{a_id}")
+    assert resp.status_code == 200
+    assert decode_json(resp) == analysis
