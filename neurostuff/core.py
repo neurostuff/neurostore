b730713efe97246f4f2cea16f3dfa3f3e5054d7c
James Kent <jamesdkent21@gmail.com>
2021-01-13 02:00:15 -0600

    add connexion and custom resolver

--- neurostuff/core.py
+++ neurostuff/core.py
@@ -1,17 +1,31 @@
 import os
-from flask import Flask
 from flask_security import Security, SQLAlchemyUserDatastore
 from flask_dance.consumer.storage.sqla import SQLAlchemyStorage
 from flask_dance.contrib.github import make_github_blueprint
 from flask_cors import CORS
+import connexion
 
+from .resolver import MethodListViewResolver
 from .database import init_db
 from .models import User, Role, OAuth
+from .resources import bind_resources
 
 
-app = Flask(__name__)
+connexion_app = connexion.FlaskApp(__name__, specification_dir="openapi/", debug=True)
+app = connexion_app.app
+
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = init_db(app)
+
+options = {"swagger_ui": True}
+connexion_app.add_api(
+    'neurostuff-api.v1.yml',
+    options=options,
+    arguments={"title": "NeuroStore API"},
+    resolver=MethodListViewResolver("neurostuff.resources.resources"),
+    strict_validation=True,
+    validate_responses=True,
+)
 
 # Enable CORS
 cors = CORS(app, expose_headers='X-Total-Count')
@@ -21,7 +35,6 @@ user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
 # Flask-Dance (OAuth)
-from . import oauth
 app.secret_key = app.config['DANCE_SECRET_KEY']
 blueprint = make_github_blueprint(
     client_id=app.config['GITHUB_CLIENT_ID'],
@@ -38,5 +51,6 @@ blueprint.storage = SQLAlchemyStorage(OAuth, db.session)
 #                  context_value={'session': db.session}))
 
 # Bind routes
-from .resources import bind_resources
 bind_resources(app)
+
+# connexion_app.run(port=8080)
