3e2c76a388d7289ee0f76c728870dfca69517882
James Kent <jamesdkent21@gmail.com>
2021-05-21 14:05:17 -0500

    fix style and add assertions

--- neurostore/tests/api/test_user.py
+++ neurostore/tests/api/test_user.py
@@ -1,6 +1,3 @@
-from ...models.auth import User
-
-
 def test_create_user(auth_client):
     new_user = {
         'email': 'this@that.com',
@@ -9,5 +6,13 @@ def test_create_user(auth_client):
         'password': 'more than six characters'
     }
     auth_client.post("/api/register", data=new_user)
-    user = User.query.filter_by(email=new_user['email'])
-    login_resp = auth_client.post("/api/login", data={'email': new_user['email'], 'password': new_user['password']})
+    login_resp = auth_client.post(
+        "/api/login",
+        data={
+            'email': new_user['email'],
+            'password': new_user['password'],
+        }
+    )
+
+    assert login_resp.status_code == 200
+    assert 'access_token' in login_resp.json.keys()
