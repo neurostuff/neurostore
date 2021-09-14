76853c515f21b7b2c83c4eba5e221b66f0ffeecf
James Kent <jamesdkent21@gmail.com>
2021-09-14 15:48:02 -0500

    allow analyses to be nested as well

--- neurostore/tests/api/test_nested.py
+++ neurostore/tests/api/test_nested.py
@@ -1,10 +1,27 @@
 import pytest
+from ...schemas.data import StudySchema, AnalysisSchema, StringOrNested
 
 
 @pytest.mark.parametrize("nested", ['true', 'false'])
-def test_nested(auth_client, ingest_neurosynth, nested):
-    resp = auth_client.get(f"/api/studies/?nested={nested}")
-    if nested == 'true':
-        assert isinstance(resp.json['results'][0]['analyses'][0], dict)
-    else:
-        assert isinstance(resp.json['results'][0]['analyses'][0], str)
+@pytest.mark.parametrize(
+    "resource_schema",
+    [
+        ('studies', StudySchema()),
+        ('analyses', AnalysisSchema()),
+    ]
+)
+def test_nested(auth_client, ingest_neurosynth, nested, resource_schema):
+    resource, schema = resource_schema
+    resp = auth_client.get(f"/api/{resource}/?nested={nested}")
+    fields = [f for f in schema.fields if isinstance(schema.fields[f], StringOrNested)]
+    for field in fields:
+        if nested == 'true':
+            try:
+                assert isinstance(resp.json['results'][0][field][0], dict)
+            except IndexError:
+                continue
+        else:
+            try:
+                assert isinstance(resp.json['results'][0][field][0], str)
+            except IndexError:
+                continue
