2cc816395c1edb1503435b2048152cb69c1bb41b
James Kent <jamesdkent21@gmail.com>
2021-09-15 11:05:44 -0500

    test more query parameters

--- neurostore/tests/api/test_query_params.py
+++ neurostore/tests/api/test_query_params.py
@@ -25,3 +25,13 @@ def test_nested(auth_client, ingest_neurosynth, nested, resource_schema):
                 assert isinstance(resp.json['results'][0][field][0], str)
             except IndexError:
                 continue
+
+
+def test_user_id(auth_client, user_studies):
+    from ...resources.auth import decode_token
+    from ...resources.users import User
+
+    user = User.query.filter_by(external_id=decode_token(auth_client.token)['sub']).first()
+    resp = auth_client.get(f"/api/studies/?user_id={user.external_id}")
+    for study in resp.json['results']:
+        assert study['user'] == user.external_id
