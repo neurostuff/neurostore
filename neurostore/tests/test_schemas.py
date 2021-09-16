4d6fb0122b76ef460e967c66daaae3912f6725f9
James Kent <jamesdkent21@gmail.com>
2021-09-16 15:58:11 -0500

    refactor test to simplify

--- neurostore/tests/test_schemas.py
+++ neurostore/tests/test_schemas.py
@@ -4,18 +4,7 @@ from ..models import Study
 
 def test_StudySchema(auth_client, ingest_neurosynth):
     study_entry = Study.query.first()
-    payload = auth_client.get(f"/api/studies/{study_entry.id}?nested=true").json
-    payload.pop("created_at")
-    payload.pop("user")
-    payload.pop("source")
-    payload.pop("source_id")
-    payload.pop("source_updated_at")
-    payload["metadata"] = {"cool": "important detail"}
-    [(pl.pop("created_at"), pl.pop("study"), pl.pop('user')) for pl in payload["analyses"]]
-    [
-        (p.pop("created_at"), p.pop("analysis"), p.pop('user'))
-        for pl in payload["analyses"]
-        for p in pl["points"]
-    ]
+    payload = StudySchema(context={'nested': False}).dump(study_entry)
+    [payload.pop(f, None) for f in StudySchema().fields if StudySchema().fields[f].dump_only]
     StudySchema().load(payload)
     StudySchema().validate(payload)
