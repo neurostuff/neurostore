2fb6bc01a8892dc3fa7c2c55e9bef03bb44d342c
James Kent <jamesdkent21@gmail.com>
2021-11-02 10:59:39 -0500

    formatting

--- neurostore/tests/api/test_annotations.py
+++ neurostore/tests/api/test_annotations.py
@@ -4,6 +4,26 @@ from ...models.data import Dataset, Annotation
 def test_post_annotation(auth_client, ingest_neurosynth):
     dset = Dataset.query.first()
     # y for x in non_flat for y in x
-    data = [{'study': s.id, 'analysis': a.id, 'note': {'foo': a.id}} for s in dset.studies for a in s.analyses]
+    data = [
+        {'study': s.id, 'analysis': a.id, 'note': {'foo': a.id}}
+        for s in dset.studies for a in s.analyses
+    ]
     payload = {'dataset': dset.id, 'notes': data, 'name': 'mah notes'}
-    auth_client.post('/api/annotations/', data=payload)
+    resp = auth_client.post('/api/annotations/', data=payload)
+    assert resp.status_code == 200
+
+
+def test_get_annotations(auth_client, ingest_neurosynth):
+    dset = Dataset.query.first()
+    resp = auth_client.get(f'/api/annotations/?dataset_id={dset.id}')
+    assert resp.status_code == 200
+
+
+def test_clone_annotation(auth_client, ingest_neurosynth):
+    annotation_entry = Annotation.query.first()
+    resp = auth_client.post(f"/api/annotations/?source_id={annotation_entry.id}", data={})
+    assert resp.status_code == 200
+    data = resp.json
+    assert data['name'] == annotation_entry.name
+    assert data['source_id'] == annotation_entry.id
+    assert data['source'] == 'neurostore'
