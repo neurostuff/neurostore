d793147387f1b658539775145ec525b01530edff
James Kent <jamesdkent21@gmail.com>
2021-12-29 16:33:43 -0600

    test that annotation_analyses are deleted when the analysis is deleted

--- neurostore/tests/api/test_annotations.py
+++ neurostore/tests/api/test_annotations.py
@@ -1,4 +1,4 @@
-from ...models.data import Dataset
+from ...models import Dataset, User
 
 
 def test_post_annotation(auth_client, ingest_neurosynth):
@@ -23,11 +23,14 @@ def test_get_annotations(auth_client, ingest_neurosynth):
 
     annot_id = resp.json['results'][0]['id']
 
-    resp2 = auth_client.get(f"/api/annotations/{annot_id}?export=true")
+    annot = auth_client.get(f"/api/annotations/{annot_id}")
+    assert annot.status_code == 200
 
-    assert resp2.status_code == 200
+    annot_export = auth_client.get(f"/api/annotations/{annot_id}?export=true")
 
-    df = pd.read_csv(StringIO(resp2.json['annotation']))
+    assert annot_export.status_code == 200
+
+    df = pd.read_csv(StringIO(annot_export.json['annotation']))
 
     assert isinstance(df, pd.DataFrame)
 
@@ -40,3 +43,28 @@ def test_clone_annotation(auth_client, simple_neurosynth_annotation):
     assert data['name'] == annotation_entry.name
     assert data['source_id'] == annotation_entry.id
     assert data['source'] == 'neurostore'
+
+
+def test_single_analysis_delete(auth_client, user_data):
+    user = User.query.filter_by(name="user1").first()
+    # get relevant dataset
+    datasets = auth_client.get(f"/api/datasets/?user_id={user.external_id}")
+    dataset_id = datasets.json['results'][0]['id']
+    dataset = auth_client.get(f"/api/datasets/{dataset_id}")
+    # get relevant annotation
+    annotations = auth_client.get(f"/api/annotations/?dataset_id={dataset_id}")
+    annotation_id = annotations.json['results'][0]['id']
+    annotation = auth_client.get(f"/api/annotations/{annotation_id}")
+    # pick study to edit
+    study_id = dataset.json['studies'][0]
+    study = auth_client.get(f"/api/studies/{study_id}")
+
+    # select analysis to delete
+    analysis_id = study.json['analyses'][0]
+    auth_client.delete(f"/api/analyses/{analysis_id}")
+
+    # test if annotations were updated
+    updated_annotation = auth_client.get(f"/api/annotations/{annotation_id}")
+
+    assert updated_annotation.status_code == 200
+    assert (len(annotation.json['notes']) - 1) == (len(updated_annotation.json['notes']))
