78ce511824ef0e2b20272eaaaf344604c40d4887
James Kent <jamesdkent21@gmail.com>
2022-01-29 02:16:44 -0600

    fix type

--- neurostore/tests/api/test_annotations.py
+++ neurostore/tests/api/test_annotations.py
@@ -83,10 +83,12 @@ def test_study_removal_from_dataset(auth_client, session, user_data):
     annotation_id = annotations.json['results'][0]['id']
     annotation = auth_client.get(f"/api/annotations/{annotation_id}")
     # remove study from dataset
-    studies = dataset.json['studies']
+    dataset.json['studies'].pop()
 
     # update dataset
-    auth_client.put(f"/api/datasets/{dataset_id}", data={'studies': studies})
+    auth_client.put(
+        f"/api/datasets/{dataset_id}", data={'studies': dataset.json['studies']}
+    )
 
     # test if annotations were updated
     updated_annotation = auth_client.get(f"/api/annotations/{annotation_id}")
