96dd90763e04ae0863090ebec034793020226814
James Kent <jamesdkent21@gmail.com>
2022-03-29 11:46:11 -0500

    enumerate kernel transformer possibilities in estimator
    - give better output file name
    - rm manually created file

--- synth/neurosynth-frontend/scripts/parse_nimare_docs.py
+++ synth/neurosynth-frontend/scripts/parse_nimare_docs.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from docstring_parser.numpydoc import NumpydocParser
 import nimare.meta.cbma as nicoords
 import nimare.meta.kernel as nikern
+import nimare
 NIMARE_COORDINATE_ALGORITHMS = [
     "MKDADensity",
     "KDA",
@@ -19,39 +20,57 @@ NIMARE_KERNELS = [
     "MKDAKernel",
 ]
 
+DEFAULT_KERNELS = {
+    "MKDADensity": "MKDAKernel",
+    "MKDAChi2": "MKDAKernel",
+    "KDA": "KDAKernel",
+    "ALE": "ALEKernel",
+    "ALESubtraction": "ALEKernel",
+    "SCALE": "ALEKernel",
+}
+
+BLACKLIST_PARAMS = [
+    "n_cores",
+    "memory_limit",
+    "null_distributions_",
+    "inputs_",
+    "masker",
+    "kernel_transformer",
+]
+
 numparse = NumpydocParser()
 config = {
+    "VERSION": nimare.__version__,
     "CBMA": {},
     "IBMA": {},
-    "Kernel": {},
 }
 
 for algo in NIMARE_COORDINATE_ALGORITHMS:
     func = getattr(nicoords, algo)
     docs = numparse.parse(func.__doc__)
+
     config["CBMA"][algo] = {
         "parameters": {
             param.arg_name: {
                 "description": param.description,
                 "type": param.type_name,
-            } for param in docs.params if param.arg_name != '**kwargs'
+            } for param in docs.params if param.arg_name not in BLACKLIST_PARAMS
         }
     }
 
-for kern in NIMARE_KERNELS:
-    func = getattr(nikern, kern)
-    docs = numparse.parse(func.__doc__)
-    config["Kernel"][kern] = {
-        "parameters": {
-            param.arg_name: {
+    kern_func = getattr(nikern, DEFAULT_KERNELS[algo])
+    kern_docs = numparse.parse(kern_func.__doc__)
+    config["CBMA"][algo]['parameters'].update(
+        {
+            "kernel__" + param.arg_name: {
                 "description": param.description,
                 "type": param.type_name,
-            } for param in docs.params if param.arg_name != '**kwargs'
+            } for param in kern_docs.params if param.arg_name not in BLACKLIST_PARAMS
         }
-    }
+    )
 
 # save config file
-fname = Path(__file__).parent.parent / "src" / "assets" / "config" / "config_auto.json"
+fname = Path(__file__).parent.parent / "src" / "assets" / "config" / "meta_analysis_params.json"
 
 with open(fname, "w+") as c:
     json.dump(config, c, indent=4)
