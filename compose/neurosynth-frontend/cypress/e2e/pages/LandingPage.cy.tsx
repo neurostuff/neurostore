f3655f5e6359d8dfdaebe27a14f210003f52eb16
Nicholas Lee <niconal902@gmail.com>
2022-08-19 11:38:25 -0700

    feat: allowed viewport to shrink more without turning into hamburger menu, moved drawer to right side (#339)
    * feat: allowed viewport to shrink more without turning into hamburger menu, moved drawer to right side
    
    * chore: added e2e test for navbar resize

--- compose/neurosynth-frontend/cypress/e2e/pages/LandingPage.cy.tsx
+++ compose/neurosynth-frontend/cypress/e2e/pages/LandingPage.cy.tsx
@@ -8,4 +8,13 @@ describe(PAGE_NAME, () => {
     it('should load successfully', () => {
         cy.visit('/');
     });
+
+    it('should change the viewport property', () => {
+        // md breakpoint is at 900px
+        cy.visit('/').viewport(899, 750);
+        cy.get('[data-testid="MenuIcon"]').should('be.visible');
+        cy.contains('STUDIES').should('not.be.visible');
+        cy.contains('STUDYSETS').should('not.be.visible');
+        cy.contains('META-ANALYSES').should('not.be.visible');
+    });
 });
--- compose/neurosynth-frontend/src/components/Navbar/Navbar.styles.ts
+++ compose/neurosynth-frontend/src/components/Navbar/Navbar.styles.ts
@@ -6,30 +6,22 @@ const NavbarStyles: Style = {
         // https://stackoverflow.com/questions/8468066/child-inside-parent-with-min-height-100-not-inheriting-height
         height: '70px',
         justifyContent: 'space-between',
-        width: '80%',
+        width: {
+            xs: '90%',
+            md: '80%',
+        },
         margin: '0 auto',
     },
     mdDown: {
         display: {
             xs: 'flex',
-            lg: 'none',
+            md: 'none',
         },
     },
     mdUp: {
         display: {
             xs: 'none',
-            lg: 'flex',
-        },
-    },
-    neurosynthLink: {
-        textDecoration: 'none',
-        color: 'white',
-        textTransform: 'lowercase',
-        '&:hover': {
-            '& img': {
-                opacity: '0.8',
-            },
-            color: '#ef8a24',
+            md: 'flex',
         },
     },
 };
