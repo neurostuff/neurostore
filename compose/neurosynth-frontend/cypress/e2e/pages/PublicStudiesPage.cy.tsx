1b2e745d05fdb61995fbe66520f7dbd5dfd3b554
Nick Lee <nicholas.lee2@mail.mcgill.ca>
2022-09-03 01:02:53 -0400

    fix: updated integration tests

--- compose/neurosynth-frontend/cypress/e2e/pages/PublicStudiesPage.cy.tsx
+++ compose/neurosynth-frontend/cypress/e2e/pages/PublicStudiesPage.cy.tsx
@@ -18,6 +18,133 @@ describe(PAGE_NAME, () => {
         cy.login('real').wait('@realStudiesRequest').visit(PATH).wait('@realStudiesRequest');
     });
 
+    describe('Search', () => {
+        beforeEach(() => {
+            cy.intercept('GET', `**/api/studies/*`, {
+                metadata: { total_count: 1000, unique_count: 1000 },
+                results: [...mockStudies(), ...mockStudies(), ...mockStudies()],
+            }).as('studiesRequest');
+        });
+
+        it('should make a correct request after selecting a different page of results', () => {
+            cy.login('mocked')
+                // ARRANGE
+                .wait('@studiesRequest')
+                .visit(PATH)
+                .wait('@studiesRequest')
+                .get('button')
+                .contains('5')
+                // ACT
+                .click()
+                .wait('@studiesRequest')
+                // ASSERT
+                .its('request.url')
+                .should('contain', 'page=5');
+        });
+
+        it('should make a correct request when selecting a different number of items to display on a single page', () => {
+            cy.login('mocked')
+                // ARRANGE
+                .wait('@studiesRequest')
+                .visit(PATH)
+                .wait('@studiesRequest')
+                .get('body')
+                .contains('Rows per page')
+                .siblings()
+                .eq(1)
+                // ACT
+                .click()
+                .get('[role="option"]')
+                .contains('99')
+                .click()
+                // ASSERT
+                .wait('@studiesRequest')
+                .its('request.url')
+                .should('contain', 'page_size=99');
+        });
+
+        it('should make a correct request when searching via the "All" option', () => {
+            cy.login('mocked')
+                // ARRANGE
+                .wait('@studiesRequest')
+                .visit(PATH)
+                .wait('@studiesRequest')
+                .get('input')
+                .eq(1)
+                // ACT
+                .type('abcdefg{enter}')
+                .wait('@studiesRequest')
+                .its('request.url')
+                .should('contain', 'search=abcdefg');
+        });
+
+        it('should make a correct request when searching via the "Title" option', () => {
+            cy.login('mocked')
+                // ARRANGE
+                .wait('@studiesRequest')
+                .visit(PATH)
+                .wait('@studiesRequest')
+                .get('body')
+                .contains('All')
+                // ACT
+                .click()
+                .get('[role="option"]')
+                .contains('Title')
+                .click()
+                .get('input')
+                .eq(1)
+                // ASSERT
+                .type('abcdefg{enter}')
+                .wait('@studiesRequest')
+                .its('request.url')
+                .should('contain', 'name=abcdefg');
+        });
+
+        it('should make a correct request when searching via the "Authors" option', () => {
+            cy.login('mocked')
+                // ARRANGE
+                .wait('@studiesRequest')
+                .visit(PATH)
+                .wait('@studiesRequest')
+                .get('body')
+                .contains('All')
+                // ACT
+                .click()
+                .get('[role="option"]')
+                .contains('Authors')
+                .click()
+                .get('input')
+                .eq(1)
+                // ASSERT
+                .type('abcdefg{enter}')
+                .wait('@studiesRequest')
+                .its('request.url')
+                .should('contain', 'authors=abcdefg');
+        });
+
+        it.only('should make a correct request when searching via the "Description" option', () => {
+            cy.login('mocked')
+                // ARRANGE
+                .wait('@studiesRequest')
+                .visit(PATH)
+                .wait('@studiesRequest')
+                .get('body')
+                .contains('All')
+                // ACT
+                .click()
+                .get('[role="option"]')
+                .contains('Description')
+                .click()
+                .get('input')
+                .eq(1)
+                // ASSERT
+                .type('abcdefg{enter}')
+                .wait('@studiesRequest')
+                .its('request.url')
+                .should('contain', 'description=abcdefg');
+        });
+    });
+
     describe('Tour ', () => {
         beforeEach(() => {
             cy.intercept('GET', `**/api/studies/*`, { results: mockStudies() }).as(
