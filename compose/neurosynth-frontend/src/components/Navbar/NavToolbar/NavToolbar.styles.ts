b1285031d0f3226c0cf99aad24b1f958f8aa06bd
Nicholas Lee <niconal902@gmail.com>
2022-11-16 21:39:34 -0500

    chore: remove unused navbar code

--- compose/neurosynth-frontend/src/components/Navbar/NavToolbar/NavToolbar.styles.ts
+++ compose/neurosynth-frontend/src/components/Navbar/NavToolbar/NavToolbar.styles.ts
@@ -22,14 +22,6 @@ const NavToolbarStyles: Style = {
             xl: '1rem',
         },
     },
-    link: {
-        textDecoration: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        color: 'primary.contrastText',
-        padding: '0 8px',
-        whiteSpace: 'nowrap',
-    },
 };
 
 export default NavToolbarStyles;
