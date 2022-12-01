f132b1038d9e64fd000525644f84ea928da45190
James Kent <jamesdkent21@gmail.com>
2022-12-01 14:16:08 -0600

    modify test to represent meta_analysis_result

--- compose/neurosynth_compose/tests/api/test_meta_analysis_result.py
+++ compose/neurosynth_compose/tests/api/test_meta_analysis_result.py
@@ -2,14 +2,20 @@ from pathlib import Path
 
 from ...models import MetaAnalysis, User
 
-def test_meta_analysis_result(app, auth_client, user_data, mock_pynv):
+def test_meta_analysis_result(app, auth_client, user_data, meta_analysis_results, mock_pynv):
     user = User.query.filter_by(name="user1").first()
     meta_analysis = MetaAnalysis.query.filter_by(user=user).first()
-    data = {"meta_analysis_id": meta_analysis.id}
-    post_collection = auth_client.post("/api/neurovault-collections", data=data)
-    assert post_collection.status_code == 200
-    upload_dir = Path(
-            app.config['FILE_DIR']
-    ) / 'uploads' / str(post_collection.json['collection_id'])
-
-    assert upload_dir.exists()  
+    results = meta_analysis_results[user.id]['results']
+    map_names = results.maps.keys()
+    data = {
+        "neurovault_collection":
+        {
+                "files":
+                [
+                        {'name': k, 'file': str(results.get_map(k).to_bytes().decode('latin1'))} for k in map_names
+                ]
+        }
+    }
+    data["meta_analysis_id"] = meta_analysis.id
+    post_result = auth_client.post("/api/meta-analysis-results", data=data)
+    assert post_result.status_code == 200
