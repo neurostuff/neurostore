b0a2e0ef8d615e120329d834d6fa2ff62f544125
James Kent <jamesdkent21@gmail.com>
2023-01-31 21:50:50 -0600

    re-work task to function with real neurovault

--- compose/neurosynth_compose/resources/tasks.py
+++ compose/neurosynth_compose/resources/tasks.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+import shutil
+from tempfile import mkdtemp
+
+from nibabel import Nifti1Image
+
 from ..core import celery_app
 from ..database import init_db
 from ..models import NeurovaultFile, MetaAnalysis, NeurovaultCollection
@@ -7,28 +13,33 @@ from ..models import NeurovaultFile, MetaAnalysis, NeurovaultCollection
 def file_upload_neurovault(self, data, id):
     from pynv import Client
     from flask import current_app as app
+
     db = init_db(app)
-    meta = MetaAnalysis.query.first()
-    if meta is None:
-        raise ValueError(f"{db}")
-    coll = NeurovaultCollection.query.first()
-    
     record = NeurovaultFile.query.filter_by(id=id).one()
     api = Client(access_token=app.config['NEUROVAULT_ACCESS_TOKEN'])
 
     try:
+        tmp_dir = Path(mkdtemp())
+        filename = f"{data['collection_id']}_{data['name']}.nii"
+        file_path = tmp_dir / filename
+        nii = Nifti1Image.from_bytes(data['file'].encode('latin1'))
+        nii.to_filename(file_path)
+
         nv_file = api.add_image(
-            data['collection_id'], data['file'],
-            modality="fMRI-BOLD", map_type=data.get("map_type", None),
-            analysis_level="G", cognitive_paradigm_cogatlas='None',
-            is_valid=True)
+            data['collection_id'], file_path.as_posix(),
+            modality="fMRI-BOLD", map_type=data.get("map_type", 'Other'),
+            analysis_level="G",
+            is_valid=True, name=data['name'])
         
         data.pop('file') # remove file as it's been uploaded
         data['image_id'] = nv_file['id']
-        data['status'] = "SUCCESS"
+        data['status'] = "OK"
+
+        # remove directory and file
+        shutil.rmtree(tmp_dir)
         
     except:
-        data['status'] = "FAILURE"
+        data['status'] = "FAILED"
     
     for k, v in data.items():
         setattr(record, k, v)
