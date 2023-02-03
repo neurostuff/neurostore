b0ccf0c49a85b54aca5293534e70283627747e1b
Nicholas Lee <niconal902@gmail.com>
2023-02-03 00:16:11 -0500

    feat: added prisma component

---
+++ compose/neurosynth-frontend/src/components/PrismaComponent/PrismaComponent.tsx
@@ -0,0 +1,92 @@
+export interface IPRISMAWorkflow {
+    identification: {
+        recordsIdentified: {
+            databaseName: string;
+            numRecords: number;
+            type: 'DATABASE' | 'REGISTER';
+        }[];
+        duplicateRecordsRemoved: number;
+        exclusions: {
+            exclusionReason: string;
+            numRecords: number;
+        }[];
+    };
+    screening: {
+        numRecordsToScreen: number; // number of records identified minus number from duplicates removed
+        exclusions: {
+            // excluded based on title and abstract - not english, not relevant, etc
+            exclusionReason: string;
+            numRecords: number;
+        }[];
+        recordsSoughtForRetrieval: number; // number of numRecordsToScreen minus number of excluded records
+        recordsNotRetrieved: number; // number of records where user is unable to retrieve full text
+    };
+    eligibility: {
+        recordsAssessedForEligibility: number; // number of recordsSoughtForRetrieval minus recordsNotRetrieved
+        exclusions: {
+            exclusionReason: string;
+            numRecords: number;
+        }[];
+    };
+    included: {
+        recordsIncluded: number; // recordsAssessedForEligibility minus number of records excluded during eligibility
+    };
+}
+
+const prismaExample: IPRISMAWorkflow = {
+    identification: {
+        recordsIdentified: [
+            {
+                databaseName: 'PubMed',
+                numRecords: 2208,
+                type: 'DATABASE',
+            },
+            {
+                databaseName: 'OTHER',
+                numRecords: 9,
+                type: 'DATABASE',
+            },
+        ],
+        duplicateRecordsRemoved: 15,
+        exclusions: [],
+    },
+    screening: {
+        numRecordsToScreen: 2202,
+        exclusions: [
+            {
+                exclusionReason: 'irrelevant',
+                numRecords: 2115,
+            },
+        ],
+        recordsSoughtForRetrieval: 87,
+        recordsNotRetrieved: 0,
+    },
+    eligibility: {
+        recordsAssessedForEligibility: 87,
+        exclusions: [
+            {
+                exclusionReason: 'out of scope',
+                numRecords: 44,
+            },
+            {
+                exclusionReason: 'insufficient details',
+                numRecords: 3,
+            },
+            {
+                exclusionReason: 'limited rigor',
+                numRecords: 3,
+            },
+        ],
+    },
+    included: {
+        recordsIncluded: 37,
+    },
+};
+
+const PrismaComponent: React.FC<{ prisma?: IPRISMAWorkflow }> = (props) => {
+    const { prisma = prismaExample } = props;
+
+    return <div>hello hello hello</div>;
+};
+
+export default PrismaComponent;
--- compose/neurosynth-frontend/src/pages/CurationPage/CurationPage.tsx
+++ compose/neurosynth-frontend/src/pages/CurationPage/CurationPage.tsx
@@ -2,6 +2,7 @@ import { Box, Breadcrumbs, Button, Link, Typography } from '@mui/material';
 import { useParams, NavLink } from 'react-router-dom';
 import AddIcon from '@mui/icons-material/Add';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
+import SchemaIcon from '@mui/icons-material/Schema';
 import CurationBoard from 'components/CurationComponents/CurationBoard/CurationBoard';
 import { useState } from 'react';
 import useGetProjectById from 'hooks/requests/useGetProjectById';
@@ -9,6 +10,7 @@ import StateHandlerComponent from 'components/StateHandlerComponent/StateHandler
 import CreateStubStudyDialog from 'components/Dialogs/CreateStubStudyDialog/CreateStubStudyDialog';
 import PubmedImportDialog from 'components/Dialogs/PubMedImportDialog/PubMedImportDialog';
 import { useIsFetching, useIsMutating } from 'react-query';
+import PrismaComponent from 'components/PrismaComponent/PrismaComponent';
 
 const CurationPage: React.FC = (props) => {
     const [createStudyDialogIsOpen, setCreateStudyDialogIsOpen] = useState(false);
@@ -59,6 +61,13 @@ const CurationPage: React.FC = (props) => {
                         )}
                     </Box>
                     <Box sx={{ marginRight: '1rem' }}>
+                        <Button
+                            sx={{ marginRight: '1rem' }}
+                            variant="outlined"
+                            endIcon={<SchemaIcon />}
+                        >
+                            PRISMA diagram
+                        </Button>
                         <CreateStubStudyDialog
                             onCloseDialog={() => setCreateStudyDialogIsOpen(false)}
                             isOpen={createStudyDialogIsOpen}
@@ -86,6 +95,9 @@ const CurationPage: React.FC = (props) => {
                 </Box>
                 <Box sx={{ height: '100%', overflow: 'hidden' }}>
                     <CurationBoard />
+                </Box>
+                <Box>
+                    <PrismaComponent />
                 </Box>
             </Box>
         </StateHandlerComponent>
