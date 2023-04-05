1305ecaa9f9c6eaefd18d623eac50c4e2b2a5786
Nicholas Lee <niconal902@gmail.com>
2023-04-05 19:35:59 -0400

    feat: add validation state

--- compose/neurosynth-frontend/src/components/EditStudyComponents/EditAnalyses/EditAnalysisPoints/EditAnalysisPoints.tsx
+++ compose/neurosynth-frontend/src/components/EditStudyComponents/EditAnalyses/EditAnalysisPoints/EditAnalysisPoints.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { HotTable } from '@handsontable/react';
-import { CellChange, ChangeSource, RangeType } from 'handsontable/common';
+import { CellChange, CellValue, ChangeSource, RangeType } from 'handsontable/common';
 import { registerAllModules } from 'handsontable/registry';
 import { Settings } from 'handsontable/plugins/contextMenu';
 import {
@@ -17,6 +17,15 @@ import { IStorePoint } from 'pages/Studies/StudyStore';
 export const ROW_HEIGHT = 56;
 
 registerAllModules();
+
+// const nonEmptyNumericValidator = (value: CellValue, callback: (isValid: boolean) => void) => {
+//     const isNumber = !isNaN(value);
+//     if (isNumber && value !== 'e') {
+//         callback(true);
+//     } else {
+//         callback(false);
+//     }
+// };
 
 const hotTableColHeaders = ['X', 'Y', 'Z', 'Kind', 'Space'];
 const hotTableColumnSettings: ColumnSettings[] = [
@@ -81,6 +90,12 @@ const handleBeforePaste = (data: any[][], coords: RangeType[]) => {
     return true;
 };
 
+/**
+ * DO HANDS ON TABLE VALIDATIONS AND ADD isValid TO THE STUDYSTORE METADATA
+ * on each point update, do a calculation to check if there are empty coordinate cells. If so, set invalid styling.
+ * Debounce this calculation
+ */
+
 const EditAnalysisPoints: React.FC<{ analysisId?: string }> = React.memo((props) => {
     const points = useStudyAnalysisPoints(props.analysisId) as IStorePoint[];
     console.log(points);
@@ -89,11 +104,11 @@ const EditAnalysisPoints: React.FC<{ analysisId?: string }> = React.memo((props)
     const deletePoints = useDeleteAnalysisPoints();
     const hotTableRef = useRef<HotTable>(null);
 
+    const [hotTableIsValid, setHotTableIsValid] = useState(false);
+
     const handleAfterChange = (changes: CellChange[] | null, source: ChangeSource) => {
         if (!props.analysisId) return;
         if (!changes) return;
-
-        console.log({ changes, source });
 
         const updatedPoints = [...points];
         changes.forEach((change) => {
@@ -108,12 +123,14 @@ const EditAnalysisPoints: React.FC<{ analysisId?: string }> = React.memo((props)
 
         updatePoints(props.analysisId, updatedPoints);
 
+        // if the index is greater than the array size, new elements will be added into the array.
+        // we use this to our advantage to determine if rows were copy pasted
         if (source === 'CopyPaste.paste' && updatedPoints.length > points.length) {
             const pointsToCreate = updatedPoints.slice(points.length, updatedPoints.length);
             createPoint(props.analysisId, pointsToCreate, points.length);
         }
 
-        return false;
+        return;
     };
 
     const handleBeforeRemoveRow = (
@@ -130,14 +147,28 @@ const EditAnalysisPoints: React.FC<{ analysisId?: string }> = React.memo((props)
         return false;
     };
 
+    const handleAfterValidate = (
+        isValid: boolean,
+        value: any,
+        row: number,
+        prop: string | number,
+        source: ChangeSource
+    ) => {
+        console.log({
+            isValid,
+            value,
+            row,
+            prop,
+            source,
+        });
+    };
+
     const handleBeforeCreateRow = (
         index: number,
         amount: number,
         source?: ChangeSource | undefined
     ) => {
         if (!props.analysisId) return false;
-
-        console.log({ index, amount, source });
 
         if (source === 'ContextMenu.rowAbove' || source === 'ContextMenu.rowBelow') {
             const addedRowIndex = source === 'ContextMenu.rowAbove' ? index - 1 : index + 1;
@@ -168,6 +199,7 @@ const EditAnalysisPoints: React.FC<{ analysisId?: string }> = React.memo((props)
                 beforePaste={handleBeforePaste}
                 beforeRemoveRow={handleBeforeRemoveRow}
                 beforeCreateRow={handleBeforeCreateRow}
+                afterValidate={handleAfterValidate}
                 stretchH="all"
                 allowRemoveColumn={false}
                 allowInvalid={false}
