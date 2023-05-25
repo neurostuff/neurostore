3cab0cc46e7835c489ad1a924f11b6c311f99f0a
James Kent <jamesdkent21@gmail.com>
2023-05-25 12:26:47 -0500

    add prance for rendering of spec

--- store/neurostore/core.py
+++ store/neurostore/core.py
@@ -5,7 +5,9 @@ from authlib.integrations.flask_client import OAuth
 import connexion
 from connexion.json_schema import default_handlers as json_schema_handlers
 from connexion.resolver import MethodViewResolver
+from connexion import spec
 from flask_cors import CORS
+import prance
 
 from .or_json import ORJSONDecoder, ORJSONEncoder
 from .database import init_db
@@ -33,8 +35,19 @@ json_schema_handlers[""] = lambda uri: (
     json_schema_handlers["file"](str(connexion_app.specification_dir / uri))
 )
 
+
+# https://github.com/spec-first/connexion/issues/254#issuecomment-504699959
+def get_bundled_specs(main_file):
+    parser = prance.ResolvingParser(str(main_file.absolute()),
+                                    lazy=True, backend='openapi-spec-validator')
+    parser.parse()
+    return parser.specification
+
+
+openapi_file = Path(os.path.dirname(__file__) + "/openapi/neurostore-openapi.yml")
+
 connexion_app.add_api(
-    Path(os.path.dirname(__file__) + "/openapi/neurostore-openapi.yml"),
+    get_bundled_specs(openapi_file),
     base_path="/api",
     options=options,
     arguments={"title": "NeuroStore API"},
