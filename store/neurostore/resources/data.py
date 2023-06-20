df4e697bb5b77a1779b6d1a0673b0562aca9e936
James Kent <jamesdkent21@gmail.com>
2023-06-20 17:06:20 -0500

    preload studyset_studies table

--- store/neurostore/resources/data.py
+++ store/neurostore/resources/data.py
@@ -250,6 +250,7 @@ class StudiesView(ObjectView, ListView):
     def join_tables(self, q):
         "join relevant tables to speed up query"
         q = q.options(joinedload('analyses'))
+        q = q.options(joinedload('studyset_studies'))
 
         return q
 
