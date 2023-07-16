53f42f42af4b79c5598a001c49350ec15972552e
James Kent <jamesdkent21@gmail.com>
2023-07-16 15:55:28 -0500

    mount data files for ingestion

--- store/scripts/ace_ingestion.py
+++ store/scripts/ace_ingestion.py
@@ -1,8 +1,8 @@
 from neurostore.ingest import load_ace_files, ace_ingestion_logic
 
-coordinates_file = "/media/data-drive/ace/no-pubmed-central-july-11-2023/coordinates.csv"
-metadata_file = "/media/data-drive/ace/no-pubmed-central-july-11-2023/metadata.csv"
-text_file = "/media/data-drive/ace/no-pubmed-central-july-11-2023/text.csv"
+coordinates_file = "/ace/no-pubmed-central-july-11-2023/coordinates.csv"
+metadata_file = "/ace/no-pubmed-central-july-11-2023/metadata.csv"
+text_file = "/ace/no-pubmed-central-july-11-2023/text.csv"
 
 coordinates_df, metadata_df, text_df = load_ace_files(coordinates_file, metadata_file, text_file)
 
