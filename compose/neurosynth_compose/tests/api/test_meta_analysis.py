0328d89c00f675e33a59eec37c09467ff6aa8586
James Kent <jamesdkent21@gmail.com>
2024-01-31 23:41:52 -0600

    update logic and test

--- compose/neurosynth_compose/tests/api/test_meta_analysis.py
+++ compose/neurosynth_compose/tests/api/test_meta_analysis.py
@@ -1,3 +1,6 @@
+from neurosynth_compose.models import MetaAnalysis
+
+
 def test_get_meta_analyses(session, app, auth_client, user_data):
     get_all = auth_client.get("/api/meta-analyses")
     assert get_all.status_code == 200
@@ -13,6 +16,18 @@ def test_get_meta_analyses(session, app, auth_client, user_data):
     data = get_one_nested.json
     for key in ["specification", "studyset", "annotation"]:
         assert data[key] is None or isinstance(data[key], dict)
+
+
+def test_get_specific_meta_analyses(session, app, auth_client, user_data):
+    metas = MetaAnalysis.query.limit(3).all()
+    ids = set([m.id for m in metas])
+    ids_str = "&".join([f"ids={m.id}" for m in metas])
+    get_all = auth_client.get(f"/api/meta-analyses?{ids_str}")
+    assert get_all.status_code == 200
+
+    return_ids = set([m["id"] for m in get_all.json["results"]])
+
+    assert ids == return_ids
 
 
 def test_ingest_neurostore(session, neurostore_data):
