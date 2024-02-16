3cec383c1afde37bc789ada7cf9022ed716df431
Nicholas Lee <niconal902@gmail.com>
2024-02-16 17:58:27 -0500

    fix: align display analyses list component

--- compose/neurosynth-frontend/src/components/DisplayStudy/DisplayAnalyses/DisplayAnalyses.tsx
+++ compose/neurosynth-frontend/src/components/DisplayStudy/DisplayAnalyses/DisplayAnalyses.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@mui/material';
 import DisplayAnalysesList from './DisplayAnalysesList/DisplayAnalysesList';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import DisplayAnalysis from './DisplayAnalysis/DisplayAnalysis';
 import { IStoreAnalysis } from 'pages/Studies/StudyStore.helpers';
 
@@ -8,32 +8,38 @@ const DisplayAnalyses: React.FC<{
     id: string | undefined;
     analyses: IStoreAnalysis[];
 }> = (props) => {
-    const [selectedAnalysisIndex, setSelectedAnalysisIndex] = useState(0);
+    const [selectedAnalysisId, setSelectedAnalysisId] = useState<string | undefined>('');
 
     useEffect(() => {
-        setSelectedAnalysisIndex(0);
-    }, [props.id]);
+        if (props.analyses.length <= 0) return;
+        setSelectedAnalysisId(props.analyses[0].id);
+    }, [props.analyses, props.id]);
 
-    const handleSelectAnalysis = (index: number) => {
-        if (props.analyses[index]) {
-            setSelectedAnalysisIndex(index);
+    const handleSelectAnalysis = (id: string) => {
+        const index = props.analyses.findIndex((a) => a.id === id);
+        if (index < 0) {
+            return;
         } else {
-            setSelectedAnalysisIndex(0);
+            setSelectedAnalysisId(props.analyses[index].id);
         }
     };
 
-    const selectedAnalysis = props.analyses[selectedAnalysisIndex];
+    const selectedAnalysis = useMemo(() => {
+        return props.analyses.find((a) => a.id === selectedAnalysisId);
+    }, [props.analyses, selectedAnalysisId]);
 
     return (
         <Box sx={{ display: 'flex' }}>
             <DisplayAnalysesList
-                selectedIndex={selectedAnalysisIndex}
+                selectedId={selectedAnalysisId}
                 onSelectAnalysisIndex={handleSelectAnalysis}
                 analyses={props.analyses}
             />
-            <Box sx={{ padding: '1rem', width: 'calc(100% - 250px - 2rem)', height: '100%' }}>
-                <DisplayAnalysis {...selectedAnalysis} />
-            </Box>
+            {selectedAnalysis && (
+                <Box sx={{ padding: '1rem', width: 'calc(100% - 250px - 2rem)', height: '100%' }}>
+                    <DisplayAnalysis {...selectedAnalysis} />
+                </Box>
+            )}
         </Box>
     );
 };
