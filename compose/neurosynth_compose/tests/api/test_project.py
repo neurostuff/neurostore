8fb7f12e2c5d9a25a0abf529436885de5d3b3256
James Kent <jamesdkent21@gmail.com>
2024-02-28 20:55:52 -0600

    fix style/add missing file

--- compose/neurosynth_compose/tests/api/test_project.py
+++ compose/neurosynth_compose/tests/api/test_project.py
@@ -1,18 +1,24 @@
 from neurosynth_compose.models import Project
 from neurosynth_compose.schemas import MetaAnalysisSchema
 
+
 def test_get_all_projects(session, app, auth_client, user_data):
     projects = Project.query.all()
 
     project_ids = set(
-            [m.id for m in projects if m.public or getattr(m.user, "external_id", None) == auth_client.username]
-        )
-    
+        [
+            m.id
+            for m in projects
+            if m.public or getattr(m.user, "external_id", None) == auth_client.username
+        ]
+    )
+
     returned_projects = auth_client.get("/api/projects")
     assert returned_projects.status_code == 200
-    returned_ids = set([p['id'] for p in returned_projects.json['results']])
+    returned_ids = set([p["id"] for p in returned_projects.json["results"]])
 
     assert project_ids == returned_ids
+
 
 def test_project_info(session, app, auth_client, user_data):
     proj = Project.query.first()
