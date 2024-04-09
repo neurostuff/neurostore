6b10a5aff11cd79ed2732857c9abca37132fb20b
James Kent <jamesdkent21@gmail.com>
2024-04-09 15:39:10 -0500

    fix style

--- compose/neurosynth_compose/tests/api/test_project.py
+++ compose/neurosynth_compose/tests/api/test_project.py
@@ -9,7 +9,8 @@ def test_get_all_projects(session, app, auth_client, user_data):
         [
             m.id
             for m in projects
-            if (m.public and not m.draft) or getattr(m.user, "external_id", None) == auth_client.username
+            if (m.public and not m.draft)
+            or getattr(m.user, "external_id", None) == auth_client.username
         ]
     )
 
