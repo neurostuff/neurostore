be2152398f44601af37388351eda6557eab1755f
Nicholas Lee <niconal902@gmail.com>
2025-07-22 19:30:03 -0500

    maint: added source map configs

--- compose/neurosynth-frontend/vite.config.ts
+++ compose/neurosynth-frontend/vite.config.ts
@@ -1,21 +1,36 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    plugins: [react(), viteTsconfigPaths()],
+    plugins: [
+        react(),
+        viteTsconfigPaths(),
+        sentryVitePlugin({
+            org: 'neurosynth',
+            project: 'javascript-react',
+        }),
+    ],
+
     server: {
         host: 'localhost',
         open: true,
         port: 3000,
     },
+
     test: {
         globals: true,
         setupFiles: 'src/setupTests.ts',
         environment: 'jsdom',
     },
+
     preview: {
         port: 3000,
         host: true,
+    },
+
+    build: {
+        sourcemap: true,
     },
 });
