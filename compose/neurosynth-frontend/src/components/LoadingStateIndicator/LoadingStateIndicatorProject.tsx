5877d043b04f82f9ac4a505239a0d0ad74933052
Nicholas Lee <niconal902@gmail.com>
2025-08-12 12:44:28 -0400

    feat: added support for errors occurring during debounce (#1077)

--- compose/neurosynth-frontend/src/components/LoadingStateIndicator/LoadingStateIndicatorProject.tsx
+++ compose/neurosynth-frontend/src/components/LoadingStateIndicator/LoadingStateIndicatorProject.tsx
@@ -1,13 +1,19 @@
-import { useProjectMetadataHasUnsavedchanges, useUpdateProjectIsLoading } from 'pages/Project/store/ProjectStore';
+import {
+    useProjectIsError,
+    useProjectMetadataHasUnsavedchanges,
+    useUpdateProjectIsLoading,
+} from 'pages/Project/store/ProjectStore';
 import LoadingStateIndicator from './LoadingStateIndicator';
 
 const LoadingStateIndicatorProject: React.FC<{ isLoading?: boolean }> = ({ isLoading }) => {
     const updateProjectIsLoading = useUpdateProjectIsLoading();
     const projectMetadataHasUnsavedchanges = useProjectMetadataHasUnsavedchanges();
+    const isError = useProjectIsError();
 
     return (
         <LoadingStateIndicator
             isLoading={updateProjectIsLoading || isLoading}
+            isError={isError}
             hasUnsavedchanges={projectMetadataHasUnsavedchanges}
         />
     );
--- compose/neurosynth-frontend/src/pages/Project/ProjectPage.tsx
+++ compose/neurosynth-frontend/src/pages/Project/ProjectPage.tsx
@@ -2,17 +2,12 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { Box, Chip, Tab, Tabs, Typography } from '@mui/material';
 import LoadingStateIndicatorProject from 'components/LoadingStateIndicator/LoadingStateIndicatorProject';
 import NeurosynthBreadcrumbs from 'components/NeurosynthBreadcrumbs';
-import ProjectIsLoadingText from 'components/ProjectIsLoadingText';
-import StateHandlerComponent from 'components/StateHandlerComponent/StateHandlerComponent';
 import TextEdit from 'components/TextEdit/TextEdit';
-import { useGuard } from 'hooks';
 import useUserCanEdit from 'hooks/useUserCanEdit';
 import ProjectComponentsEditPrivacyToggle from 'pages/Project/components/ProjectEditPrivacyToggle';
 import {
-    useGetProjectIsLoading,
     useProjectCreatedAt,
     useProjectDescription,
-    useProjectIsError,
     useProjectMetaAnalysisCanEdit,
     useProjectName,
     useProjectUser,
@@ -39,16 +34,12 @@ const ProjectPage: React.FC = () => {
     const updateProjectName = useUpdateProjectName();
     const updateProjectDescription = useUpdateProjectDescription();
     const metaAnalysesTabEnabled = useProjectMetaAnalysisCanEdit();
-    const getProjectIsLoading = useGetProjectIsLoading();
     const projectName = useProjectName();
     const createdAt = useProjectCreatedAt();
     const projectUser = useProjectUser();
     const projectUserName = useProjectUsername();
     const projectDescription = useProjectDescription();
     const userCanEdit = useUserCanEdit(projectUser || undefined);
-    const getProjectIsError = useProjectIsError();
-
-    useGuard('/', 'No project found with id: ' + projectId, getProjectIsError, getProjectIsLoading);
 
     const tab = useMemo(() => {
         if (!metaAnalysesTabEnabled) return 0;
@@ -56,146 +47,144 @@ const ProjectPage: React.FC = () => {
     }, [location.pathname, metaAnalysesTabEnabled]);
 
     return (
-        <StateHandlerComponent isLoading={getProjectIsLoading} isError={getProjectIsError}>
-            <Box sx={{ marginBottom: '5rem' }}>
-                <Box sx={{ marginBottom: '0.5rem', display: 'flex', alignItems: 'center' }}>
-                    {isAuthenticated && (
-                        <NeurosynthBreadcrumbs
-                            breadcrumbItems={[
-                                {
-                                    text: 'Projects',
-                                    link: '/projects',
-                                    isCurrentPage: false,
-                                },
-                                {
-                                    text: projectName || '',
-                                    link: '',
-                                    isCurrentPage: true,
-                                },
-                            ]}
-                        />
-                    )}
-                    <LoadingStateIndicatorProject />
-                </Box>
+        <Box sx={{ marginBottom: '5rem' }}>
+            <Box sx={{ marginBottom: '0.5rem', display: 'flex', alignItems: 'center' }}>
+                {isAuthenticated && (
+                    <NeurosynthBreadcrumbs
+                        breadcrumbItems={[
+                            {
+                                text: 'Projects',
+                                link: '/projects',
+                                isCurrentPage: false,
+                            },
+                            {
+                                text: projectName || '',
+                                link: '',
+                                isCurrentPage: true,
+                            },
+                        ]}
+                    />
+                )}
+                <LoadingStateIndicatorProject />
+            </Box>
 
-                <Box sx={{ marginBottom: '0.5rem' }}>
-                    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Box sx={{ marginRight: '1rem', width: '100%' }}>
-                            <TextEdit
-                                onSave={(updatedName) => updateProjectName(updatedName)}
-                                sx={{ width: '100%' }}
-                                textFieldSx={{ input: { fontSize: '1.5rem' } }}
-                                textToEdit={projectName || ''}
-                                editIconIsVisible={userCanEdit}
-                            >
-                                <Typography
-                                    sx={{
-                                        color: projectName ? 'initial' : 'warning.dark',
-                                        display: 'block',
-                                    }}
-                                    variant="h5"
-                                >
-                                    {projectName || 'No name'}
-                                </Typography>
-                            </TextEdit>
-                        </Box>
-                        <ProjectComponentsEditPrivacyToggle />
-                    </Box>
-                    <TextEdit
-                        onSave={(updatedDescription) => updateProjectDescription(updatedDescription)}
-                        textFieldSx={{ input: { fontSize: '1.25rem' } }}
-                        textToEdit={projectDescription || ''}
-                        editIconIsVisible={userCanEdit}
-                        multiline
-                    >
-                        <Typography
-                            sx={{
-                                color: projectDescription ? 'muted.main' : 'warning.dark',
-                                whiteSpace: 'pre-line',
-                            }}
-                            variant="body1"
+            <Box sx={{ marginBottom: '0.5rem' }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Box sx={{ marginRight: '1rem', width: '100%' }}>
+                        <TextEdit
+                            onSave={(updatedName) => updateProjectName(updatedName)}
+                            sx={{ width: '100%' }}
+                            textFieldSx={{ input: { fontSize: '1.5rem' } }}
+                            textToEdit={projectName || ''}
+                            editIconIsVisible={userCanEdit}
                         >
-                            {projectDescription || 'No description'}
-                        </Typography>
-                    </TextEdit>
-                    <Box
-                        sx={{
-                            marginTop: '0.2rem',
-                            display: 'flex',
-                            justifyContent: 'space-between',
-                        }}
-                    >
-                        <Box>
-                            {projectUserName && (
-                                <Chip
-                                    size="small"
-                                    label={`Owner: ${projectUserName}`}
-                                    variant="outlined"
-                                    sx={{ marginRight: '0.5rem' }}
-                                />
-                            )}
-                            {createdAt && (
-                                <Chip
-                                    size="small"
-                                    label={`Created: ${
-                                        createdAt.getMonth() + 1
-                                    }/${createdAt.getDate()}/${createdAt.getFullYear()} ${createdAt.getHours()}:${createdAt.getMinutes()}`}
-                                    variant="outlined"
-                                />
-                            )}
-                        </Box>
-                    </Box>
-                </Box>
-
-                <Box sx={{ margin: '1.5rem 0' }}>
-                    <Tabs
-                        sx={{
-                            '.MuiTabs-flexContainer': {
-                                borderBottom: '1px solid lightgray',
-                            },
-                            '.MuiButtonBase-root.Mui-selected': {
-                                backgroundColor: 'white',
-                                border: '1px solid',
-                                borderTopLeftRadius: '6px',
-                                borderTopRightRadius: '6px',
-                                borderColor: 'lightgray',
-                                borderBottom: '0px',
-                                marginBottom: '-2px',
-                            },
-                            '.MuibuttonBase-root': {},
-                            transition: 'none',
-                        }}
-                        TabIndicatorProps={{
-                            sx: {
-                                display: 'none',
-                            },
-                        }}
-                        value={tab}
-                    >
-                        <Tab
-                            onClick={() => navigate(`/projects/${projectId}/project`)}
-                            sx={{
-                                color: tab === 0 ? '#ef8a24 !important' : 'primary.main',
-                                fontWeight: tab === 0 ? 'bold' : 'normal',
-                            }}
-                            label="Project"
-                        />
-                        {metaAnalysesTabEnabled && (
-                            <Tab
-                                onClick={() => navigate(`/projects/${projectId}/meta-analyses`)}
+                            <Typography
                                 sx={{
-                                    color: tab === 1 ? '#ef8a24 !important' : 'primary.main',
-                                    fontWeight: tab === 1 ? 'bold' : 'normal',
+                                    color: projectName ? 'initial' : 'warning.dark',
+                                    display: 'block',
                                 }}
-                                label="Meta-Analyses"
+                                variant="h5"
+                            >
+                                {projectName || 'No name'}
+                            </Typography>
+                        </TextEdit>
+                    </Box>
+                    <ProjectComponentsEditPrivacyToggle />
+                </Box>
+                <TextEdit
+                    onSave={(updatedDescription) => updateProjectDescription(updatedDescription)}
+                    textFieldSx={{ input: { fontSize: '1.25rem' } }}
+                    textToEdit={projectDescription || ''}
+                    editIconIsVisible={userCanEdit}
+                    multiline
+                >
+                    <Typography
+                        sx={{
+                            color: projectDescription ? 'muted.main' : 'warning.dark',
+                            whiteSpace: 'pre-line',
+                        }}
+                        variant="body1"
+                    >
+                        {projectDescription || 'No description'}
+                    </Typography>
+                </TextEdit>
+                <Box
+                    sx={{
+                        marginTop: '0.2rem',
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                    }}
+                >
+                    <Box>
+                        {projectUserName && (
+                            <Chip
+                                size="small"
+                                label={`Owner: ${projectUserName}`}
+                                variant="outlined"
+                                sx={{ marginRight: '0.5rem' }}
                             />
                         )}
-                    </Tabs>
+                        {createdAt && (
+                            <Chip
+                                size="small"
+                                label={`Created: ${
+                                    createdAt.getMonth() + 1
+                                }/${createdAt.getDate()}/${createdAt.getFullYear()} ${createdAt.getHours()}:${createdAt.getMinutes()}`}
+                                variant="outlined"
+                            />
+                        )}
+                    </Box>
                 </Box>
-
-                <Outlet />
             </Box>
-        </StateHandlerComponent>
+
+            <Box sx={{ margin: '1.5rem 0' }}>
+                <Tabs
+                    sx={{
+                        '.MuiTabs-flexContainer': {
+                            borderBottom: '1px solid lightgray',
+                        },
+                        '.MuiButtonBase-root.Mui-selected': {
+                            backgroundColor: 'white',
+                            border: '1px solid',
+                            borderTopLeftRadius: '6px',
+                            borderTopRightRadius: '6px',
+                            borderColor: 'lightgray',
+                            borderBottom: '0px',
+                            marginBottom: '-2px',
+                        },
+                        '.MuibuttonBase-root': {},
+                        transition: 'none',
+                    }}
+                    TabIndicatorProps={{
+                        sx: {
+                            display: 'none',
+                        },
+                    }}
+                    value={tab}
+                >
+                    <Tab
+                        onClick={() => navigate(`/projects/${projectId}/project`)}
+                        sx={{
+                            color: tab === 0 ? '#ef8a24 !important' : 'primary.main',
+                            fontWeight: tab === 0 ? 'bold' : 'normal',
+                        }}
+                        label="Project"
+                    />
+                    {metaAnalysesTabEnabled && (
+                        <Tab
+                            onClick={() => navigate(`/projects/${projectId}/meta-analyses`)}
+                            sx={{
+                                color: tab === 1 ? '#ef8a24 !important' : 'primary.main',
+                                fontWeight: tab === 1 ? 'bold' : 'normal',
+                            }}
+                            label="Meta-Analyses"
+                        />
+                    )}
+                </Tabs>
+            </Box>
+
+            <Outlet />
+        </Box>
     );
 };
 
