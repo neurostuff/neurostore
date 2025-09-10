2989f8cb5343b9d92d62328c440a6bdbf62de726
James Kent <jamesdkent21@gmail.com>
2025-09-10 09:43:03 -0500

    remove leading db. where appropriate

--- compose/backend/neurosynth_compose/models/auth.py
+++ compose/backend/neurosynth_compose/models/auth.py
@@ -1,40 +1,43 @@
 from flask_security import UserMixin, RoleMixin, SQLAlchemyUserDatastore
+from sqlalchemy import Table, Column, Text, ForeignKey, String, Boolean
+from sqlalchemy.orm import relationship, backref
 
-from .analysis import db, BaseMixin
+from neurosynth_compose.database import db
+from .analysis import BaseMixin
 
 
-roles_users = db.Table(
+roles_users = Table(
     "roles_users",
-    db.Model.metadata,
-    db.Column("user_id", db.Text, db.ForeignKey("users.id")),
-    db.Column("role_id", db.Text, db.ForeignKey("roles.id")),
+    db.metadata,
+    Column("user_id", Text, ForeignKey("users.id")),
+    Column("role_id", Text, ForeignKey("roles.id")),
 )
 
 
 class Role(BaseMixin, db.Model, RoleMixin):
     __tablename__ = "roles"
 
-    name = db.Column(db.Text, unique=True)
-    description = db.Column(db.Text)
-    users = db.relationship(
+    name = Column(Text, unique=True)
+    description = Column(Text)
+    users = relationship(
         "User", secondary=roles_users, back_populates="roles", lazy="dynamic"
     )
 
 
 class User(BaseMixin, db.Model, UserMixin):
     __tablename__ = "users"
-    active = db.Column(db.Boolean())
-    name = db.Column(db.Text)
-    external_id = db.Column(db.Text, unique=True)
-    roles = db.relationship("Role", secondary=roles_users, back_populates="users")
+    active = Column(Boolean())
+    name = Column(Text)
+    external_id = Column(Text, unique=True)
+    roles = relationship("Role", secondary=roles_users, back_populates="users")
 
 
 class Device(BaseMixin, db.Model):
     __tablename__ = "devices"
-    device_name = db.Column(db.String)
-    api_key = db.Column(db.String)
-    user_id = db.Column("user_id", db.Text, db.ForeignKey("users.id"))
-    user = db.relationship("User", backref=db.backref("devices"))
+    device_name = Column(String)
+    api_key = Column(String)
+    user_id = Column("user_id", Text, ForeignKey("users.id"))
+    user = relationship("User", backref=backref("devices"))
 
 
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
