473c64fd2610634f8537d79f9fdfbf276beb91f5
Nicholas Lee <niconal902@gmail.com>
2026-01-16 14:28:30 -0800

    feat: added reset password functionality

--- compose/neurosynth-frontend/src/pages/UserProfile/UserProfilePage.tsx
+++ compose/neurosynth-frontend/src/pages/UserProfile/UserProfilePage.tsx
@@ -1,13 +1,18 @@
 import { LocalStorageCache, useAuth0 } from '@auth0/auth0-react';
-import { Typography } from '@mui/material';
+import { Avatar, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import CodeSnippet from 'components/CodeSnippet/CodeSnippet';
 import NeurosynthAccordion from 'components/NeurosynthAccordion/NeurosynthAccordion';
 import { useEffect, useState } from 'react';
+import useSendResetPasswordEmail from './hooks/useSendResetPasswordEmail';
+import { useSnackbar } from 'notistack';
+import LoadingButton from 'components/Buttons/LoadingButton';
 
 const UserProfilePage: React.FC = () => {
     const { user } = useAuth0();
     const [refreshToken, setRefreshToken] = useState('');
+    const { mutate: sendResetPasswordEmail, isLoading } = useSendResetPasswordEmail();
+    const { enqueueSnackbar } = useSnackbar();
 
     useEffect(() => {
         const localStorageCache = new LocalStorageCache();
@@ -20,19 +25,46 @@ const UserProfilePage: React.FC = () => {
         }
     }, []);
 
+    const handleResetPassword = () => {
+        const userEmail = user?.email;
+        if (!userEmail) {
+            enqueueSnackbar('User email not found', { variant: 'error' });
+            return;
+        }
+        sendResetPasswordEmail(userEmail, {
+            onSuccess: () => {
+                enqueueSnackbar('Reset password email sent', { variant: 'success' });
+            },
+            onError: () => {
+                enqueueSnackbar('Error sending reset password email', { variant: 'error' });
+            },
+        });
+    };
+
     return (
         <Box>
             <Box sx={{ display: 'flex', marginBottom: '1rem' }}>
-                <Typography variant="h4">User Profile</Typography>
+                <Avatar src={user?.picture} sx={{ width: 60, height: 60 }} />
+                <Box sx={{ marginLeft: '1rem' }}>
+                    <Typography variant="h5">{user?.name ?? user?.nickname}</Typography>
+                    <Typography variant="body1" color="text.secondary">
+                        {user?.email}
+                    </Typography>
+                </Box>
             </Box>
-            <Box>
+            <Box
+                sx={{
+                    display: 'grid',
+                    gridTemplateColumns: '150px 1fr',
+                    gap: '1rem',
+                }}
+            >
                 <Typography variant="h6" sx={{ width: '150px', display: 'inline-block' }}>
                     User:
                 </Typography>
                 <Typography sx={{ display: 'inline' }} variant="h6">
-                    {user?.name}
+                    {user?.name ?? user?.nickname}
                 </Typography>
-                <br />
 
                 <Typography variant="h6" sx={{ width: '150px', display: 'inline-block' }}>
                     Email:
@@ -40,14 +72,28 @@ const UserProfilePage: React.FC = () => {
                 <Typography sx={{ display: 'inline' }} variant="h6">
                     {user?.email}
                 </Typography>
-                <br />
-                <br />
 
+                <Box sx={{ gridColumn: 'span 2', width: '100%' }}>
+                    <LoadingButton
+                        isLoading={isLoading}
+                        loaderColor="secondary"
+                        sx={{ width: '160px' }}
+                        text="Reset Password"
+                        variant="contained"
+                        disableElevation
+                        onClick={handleResetPassword}
+                    />
+                </Box>
+            </Box>
+            <Box mt={4}>
                 {refreshToken && (
                     <NeurosynthAccordion
-                        TitleElement={<Typography variant="h6">Refresh Token</Typography>}
+                        TitleElement={<Typography variant="h6">Advanced</Typography>}
                         expandIconColor="black"
                     >
+                        <Typography variant="body2" mb={1}>
+                            Refresh Token
+                        </Typography>
                         <CodeSnippet linesOfCode={[refreshToken]} />
                     </NeurosynthAccordion>
                 )}
