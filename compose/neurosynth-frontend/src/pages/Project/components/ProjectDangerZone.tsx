06f5a10df99c0d92c84d9cee3b2ef15960f7b72b
Nicholas Lee <niconal902@gmail.com>
2026-01-19 17:19:40 -0800

    feat: allow users to delete projects with no results attached

--- compose/neurosynth-frontend/src/pages/Project/components/ProjectDangerZone.tsx
+++ compose/neurosynth-frontend/src/pages/Project/components/ProjectDangerZone.tsx
@@ -5,13 +5,19 @@ import { useDeleteStudyset } from 'hooks';
 import useDeleteProject from 'hooks/projects/useDeleteProject';
 import useUserCanEdit from 'hooks/useUserCanEdit';
 import { useSnackbar } from 'notistack';
-import { useClearProvenance, useProjectExtractionStudysetId, useProjectUser } from 'pages/Project/store/ProjectStore';
+import {
+    useClearProvenance,
+    useProjectExtractionStudysetId,
+    useProjectMetaAnalyses,
+    useProjectUser,
+} from 'pages/Project/store/ProjectStore';
 import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
-const env = import.meta.env.VITE_APP_ENV as 'DEV' | 'STAGING' | 'PROD';
+const DangerZone: React.FC = () => {
+    const env = import.meta.env.VITE_APP_ENV as 'DEV' | 'STAGING' | 'PROD';
+    const isLocalhost = window.location.hostname === 'localhost';
 
-const DangerZone: React.FC = (props) => {
     const { projectId } = useParams<{ projectId: string }>();
     const projectUser = useProjectUser();
     const { mutate: deleteProject } = useDeleteProject();
@@ -20,9 +26,10 @@ const DangerZone: React.FC = (props) => {
     const { enqueueSnackbar } = useSnackbar();
     const { mutateAsync: deleteStudyset } = useDeleteStudyset();
     const studysetId = useProjectExtractionStudysetId();
-
+    const projectMetaAnalyses = useProjectMetaAnalyses();
+    const hasResults = projectMetaAnalyses && projectMetaAnalyses.length > 0;
+    const shouldShow = (env === 'DEV' || env === 'STAGING') && isLocalhost;
     const userCanEdit = useUserCanEdit(projectUser || undefined);
-
     const [confirmationDialogIsOpen, setConfirmationDialogIsOpen] = useState(false);
 
     const handleDeleteProject = (confirm?: boolean) => {
@@ -45,55 +52,54 @@ const DangerZone: React.FC = (props) => {
     }
 
     return (
-        <>
-            {(env === 'DEV' || env === 'STAGING') && (
-                <Box
-                    sx={{
-                        border: '1px solid',
-                        borderColor: 'error.main',
-                        borderRadius: '4px',
-                        padding: '1rem',
-                        marginTop: '2rem',
-                    }}
-                >
-                    <Typography gutterBottom variant="h6" sx={{ color: 'error.main' }}>
-                        Danger zone
-                    </Typography>
-                    <ConfirmationDialog
-                        isOpen={confirmationDialogIsOpen}
-                        rejectText="Cancel"
-                        dialogTitle="Are you sure you want to delete the project?"
-                        dialogMessage="This action cannot be undone"
-                        onCloseDialog={handleDeleteProject}
-                        confirmText="Confirm"
-                    />
+        <Box
+            sx={{
+                border: '1px solid',
+                borderColor: 'error.main',
+                borderRadius: '4px',
+                padding: '1rem',
+                marginTop: '2rem',
+            }}
+        >
+            <Typography gutterBottom variant="h6" sx={{ color: 'error.main' }}>
+                Danger zone
+            </Typography>
+            <ConfirmationDialog
+                isOpen={confirmationDialogIsOpen}
+                rejectText="Cancel"
+                dialogTitle="Are you sure you want to delete the project?"
+                dialogMessage="This action cannot be undone"
+                onCloseDialog={handleDeleteProject}
+                confirmText="Confirm"
+            />
+            <Button
+                startIcon={<ReportProblemIcon />}
+                onClick={() => setConfirmationDialogIsOpen(true)}
+                variant="contained"
+                disableElevation
+                color="error"
+                disabled={hasResults}
+            >
+                {hasResults ? 'this project has associated meta-analyses and cannot be deleted' : 'delete this project'}
+            </Button>
+            {shouldShow && (
+                <Box>
                     <Button
-                        startIcon={<ReportProblemIcon />}
-                        onClick={() => setConfirmationDialogIsOpen(true)}
-                        variant="contained"
-                        disableElevation
+                        onClick={async () => {
+                            if (studysetId) {
+                                await deleteStudyset(studysetId);
+                            }
+                            clearProvenance();
+                        }}
+                        variant="outlined"
+                        sx={{ marginTop: '1rem' }}
                         color="error"
                     >
-                        delete this project
+                        clear this project (FOR DEV PURPOSES)
                     </Button>
-                    <Box>
-                        <Button
-                            onClick={async () => {
-                                if (studysetId) {
-                                    await deleteStudyset(studysetId);
-                                }
-                                clearProvenance();
-                            }}
-                            variant="outlined"
-                            sx={{ marginTop: '1rem' }}
-                            color="error"
-                        >
-                            clear this project (FOR DEV PURPOSES)
-                        </Button>
-                    </Box>
                 </Box>
             )}
-        </>
+        </Box>
     );
 };
 
