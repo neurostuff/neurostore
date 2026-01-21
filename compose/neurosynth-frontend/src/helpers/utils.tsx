69181137691b9be164ca15cdd2c28dd9e1fd4d0f
James Kent <jamesdkent21@gmail.com>
2026-01-21 13:01:58 -0600

    fix the updated_at logic to use created_at

--- compose/neurosynth-frontend/src/helpers/utils.tsx
+++ compose/neurosynth-frontend/src/helpers/utils.tsx
@@ -2,25 +2,15 @@ export const lastUpdatedAtSortFn = (
     a: { updated_at?: string | null; created_at?: string | null },
     b: { updated_at?: string | null; created_at?: string | null }
 ): number => {
-    const dateAUpdatedAt = Date.parse(a.updated_at || '');
-    const dateBUpdatedAt = Date.parse(b.updated_at || '');
+    const dateAUpdatedAt = Date.parse(a.updated_at || a.created_at || '');
+    const dateBUpdatedAt = Date.parse(b.updated_at || b.created_at || '');
 
-    if (isNaN(dateAUpdatedAt) && dateBUpdatedAt) {
-        // if update_at A does not exist, automatically treat A as smaller
-        return -1;
-    } else if (isNaN(dateBUpdatedAt) && dateAUpdatedAt) {
-        // if update_at B does not exist, automatically treat B as smaller
-        return 1;
-    } else if (dateAUpdatedAt && dateBUpdatedAt) {
-        // if they both exist and are NOT the same, do comparison
-        return dateAUpdatedAt - dateBUpdatedAt;
-    } else {
-        // if they do not exist, compare created_at instead
-        const dateA = Date.parse(a.created_at || ''); // Date.parse('') will yield NaN
-        const dateB = Date.parse(b.created_at || ''); // Date.parse('') will yield NaN
-        if (isNaN(dateA) || isNaN(dateB)) return 0;
-        return dateA - dateB;
-    }
+    if (isNaN(dateAUpdatedAt) && isNaN(dateBUpdatedAt)) return 0;
+    if (isNaN(dateAUpdatedAt)) return -1;
+    if (isNaN(dateBUpdatedAt)) return 1;
+
+    // if they both exist and are NOT the same, do comparison
+    return dateAUpdatedAt - dateBUpdatedAt;
 };
 
 /**
