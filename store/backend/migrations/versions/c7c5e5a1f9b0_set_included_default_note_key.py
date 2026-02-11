6a1e3a1c55316453c98531e0f9a6c5a1aa2d5cc5
James Kent <jamesdkent21@gmail.com>
2026-02-11 12:44:17 -0600

    fix the alembic head

--- store/backend/migrations/versions/c7c5e5a1f9b0_set_included_default_note_key.py
+++ store/backend/migrations/versions/c7c5e5a1f9b0_set_included_default_note_key.py
@@ -1,7 +1,7 @@
 """set default true for included note key
 
 Revision ID: c7c5e5a1f9b0
-Revises: b7c8d9e0f1a2
+Revises: d2f4a6b8c0e2
 Create Date: 2026-01-28 00:00:00.000000
 
 """
@@ -13,7 +13,7 @@ import neurostore.models.migration_types  # noqa: F401
 
 # revision identifiers, used by Alembic.
 revision = 'c7c5e5a1f9b0'
-down_revision = 'b7c8d9e0f1a2'
+down_revision = 'd2f4a6b8c0e2'
 branch_labels = None
 depends_on = None
 
