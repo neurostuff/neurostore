abf858763212cd5d9f157b00071c20cb44798453
Nicholas Lee <niconal902@gmail.com>
2026-02-13 13:32:34 -0800

    feat: fix calculate difference logic

--- compose/neurosynth-frontend/src/pages/Extraction/ExtractionPage.helpers.ts
+++ compose/neurosynth-frontend/src/pages/Extraction/ExtractionPage.helpers.ts
@@ -1,4 +1,4 @@
-import { StudyReturn, StudysetReturnRelationshipsStudies } from 'neurostore-typescript-sdk';
+import { StudyReturn } from 'neurostore-typescript-sdk';
 import { ICurationStubStudy } from 'pages/Curation/Curation.types';
 
 // returns bool representing whether or not there is a difference between the curation included studies and what is currently in the studyset
@@ -6,14 +6,35 @@ export const hasDifferenceBetweenStudysetAndCuration = (
     curationStubs: ICurationStubStudy[],
     studysetStudies: StudyReturn[]
 ): boolean => {
+    const curationSet = new Set();
+    curationStubs.forEach((curationStub) => {
+        if (curationStub.title) curationSet.add((curationStub.title ?? '').toLocaleLowerCase());
+        if (curationStub.pmid) curationSet.add(curationStub.pmid.toLocaleLowerCase());
+        if (curationStub.doi) curationSet.add(curationStub.doi.toLocaleLowerCase());
+    });
+
     const studysetSet = new Set();
     studysetStudies.forEach((studysetStudy) => {
         if (studysetStudy.name) studysetSet.add((studysetStudy.name ?? '').toLocaleLowerCase());
-        if (studysetStudy.pmid) studysetSet.add(studysetStudy.pmid);
-        if (studysetStudy.doi) studysetSet.add(studysetStudy.doi);
+        if (studysetStudy.pmid) studysetSet.add(studysetStudy.pmid.toLocaleLowerCase());
+        if (studysetStudy.doi) studysetSet.add(studysetStudy.doi.toLocaleLowerCase());
     });
 
-    return curationStubs.some(
-        (stub) => !studysetSet.has(stub.title) && !studysetSet.has(stub.pmid) && !studysetSet.has(stub.doi)
-    );
+    const stubInCurationButNotInStudyset = curationStubs.some((stub) => {
+        const hasMatch =
+            (stub.title && studysetSet.has(stub.title.toLocaleLowerCase())) ||
+            (stub.pmid && studysetSet.has(stub.pmid.toLocaleLowerCase())) ||
+            (stub.doi && studysetSet.has(stub.doi.toLocaleLowerCase()));
+        return !hasMatch;
+    });
+
+    const studyInStudysetButNotInCuration = studysetStudies.some((study) => {
+        const hasMatch =
+            (study.name && curationSet.has(study.name.toLocaleLowerCase())) ||
+            (study.pmid && curationSet.has(study.pmid.toLocaleLowerCase())) ||
+            (study.doi && curationSet.has(study.doi.toLocaleLowerCase()));
+        return !hasMatch;
+    });
+
+    return stubInCurationButNotInStudyset || studyInStudysetButNotInCuration;
 };
