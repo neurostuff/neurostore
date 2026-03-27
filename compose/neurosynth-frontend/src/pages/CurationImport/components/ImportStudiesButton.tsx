cbefa31f46bae609e72de2fb9196cc0a04ded9af
Nicholas Lee <niconal902@gmail.com>
2026-03-27 16:39:39 -0700

    fix: merge conflicts

--- compose/neurosynth-frontend/src/pages/CurationImport/components/ImportStudiesButton.tsx
+++ compose/neurosynth-frontend/src/pages/CurationImport/components/ImportStudiesButton.tsx
@@ -2,7 +2,7 @@ import { ArrowDropDown } from '@mui/icons-material';
 import { Box, Button, ButtonProps, ListItemText, MenuItem, MenuList } from '@mui/material';
 import NeurosynthPopper from 'components/NeurosynthPopper/NeurosynthPopper';
 import { useCurationBoardGroups } from 'pages/Curation/context/CurationBoardGroupsContext';
-import { EImportMode } from 'pages/CurationImport/CurationImport.types';
+import { EImportMode } from 'pages/Curation/Curation.types';
 import { useRef, useState } from 'react';
 import ImportStudiesDialog from './ImportStudiesDialog';
 
